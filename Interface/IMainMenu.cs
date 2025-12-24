ba7e7755109d3ef228b68d5bf53bcd188133139d
Evgeny Kuznetsov <deadmaster.gm@yandex.ru>
2025-12-24 14:52:09 +0500

    Add interface IMainMenu

---
+++ Interface/IMainMenu.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NewGunShop.List;
+
+namespace NewGunShop.Interface
+{
+    internal interface IMainMenu
+    {
+        void PointConsoleOutput(DollarBalance dollarBalance);
+    }
+}
