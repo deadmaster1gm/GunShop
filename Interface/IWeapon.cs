cb591f294b7e78829d55b55c03c5ab8f6b766520
Evgeny Kuznetsov <deadmaster.gm@yandex.ru>
2025-12-24 14:49:11 +0500

    Add interface IWeapon

---
+++ Interface/IWeapon.cs
@@ -0,0 +1,9 @@
+﻿namespace NewGunShop.Interface
+{
+    internal interface IWeapon
+    {
+        string Model { get; set; }
+        string Ammo { get; set; }
+        string Price { get; set; }
+    }
+}
