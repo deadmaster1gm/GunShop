7935a4d4b3e40d8d35cdc918073e03a8d5b1b0ac
Evgeny Kuznetsov <deadmaster.gm@yandex.ru>
2025-12-24 14:59:13 +0500

    Add interface IDataDollarBalance

---
+++ Interface/IDataDollarBalance.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NewGunShop.List;
+
+namespace NewGunShop.Interface
+{
+    internal interface IDataDollarBalance
+    {
+        int _userDollarBalance { get; set; }
+        int _sellerDollarBalance { get; set; }
+        DollarBalance GetBalance();
+    }
+}
