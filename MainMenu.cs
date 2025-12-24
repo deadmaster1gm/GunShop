281f5cb77f24a0bf114125c428142799e8ab16fc
Evgeny Kuznetsov <deadmaster.gm@yandex.ru>
2025-12-24 14:56:08 +0500

    Add class MainMenu

---
+++ MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NewGunShop.Interface;
+using NewGunShop.List;
+
+namespace NewGunShop
+{
+    internal class MainMenu : IMainMenu
+    {
+        public void PointConsoleOutput(DollarBalance dollarBalance)
+        {
+            Console.WriteLine("Добро пожаловать в оружейный магазин!\n" +
+                        "Что желаете?\n\n" +
+                        "1.Купить оружие\n" +
+                        "2.Продать оружие\n" +
+                        "3.Открыть инвентарь\n" +
+                        "4.Выйти\n\n" +
+
+                        "5.Принять оружие на склад (администратор)\n" +
+                        "6.Удалить оружие со склада (администратор)\n" +
+                        "7.Посмотреть что на складе (администратор)\n" +
+                        "___________________________________________\n" +
+                        $"БАЛАНС ${dollarBalance._userDollarBalance}");
+        }
+    }
+}
+
