e539e4ccfc6f0de74fd502ddbe29c7ded810bdfb
Evgeny Kuznetsov <deadmaster.gm@yandex.ru>
2025-12-24 14:56:45 +0500

    Add class MainMenuSwitchCase

---
+++ MainMenuSwitchCase.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using NewGunShop.Interface;
+
+namespace NewGunShop
+{
+    internal class MainMenuSwitchCase : ISwitchCasePointMenu
+    {
+        private readonly string pathWeaponList = @"weaponList.json";
+        private readonly string pathWeaponListUser = @"weaponListUser.json";
+        public void SwitchCase(IDataWeaponList dataWeaponList, IInputDataProcessor inputDataProcessor)
+        {
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    Console.Clear();
+                    Console.WriteLine("Какое оружие желаете приобрести?");
+                    dataWeaponList.GetWeaponList(pathWeaponList);
+                    break;
+                case "2":
+                    Console.Clear();
+                    Console.WriteLine("Какое оружие желаете продать?");
+                    dataWeaponList.GetWeaponList(pathWeaponListUser);
+                    break;
+                case "3":
+                    Console.Clear();
+                    Console.WriteLine("Инвентарь:");
+                    dataWeaponList.GetWeaponList(pathWeaponListUser);
+                    break;
+                case "4":
+                    return;
+                case "5":
+                    dataWeaponList.AppendToWeaponList(pathWeaponList, inputDataProcessor.SetData(pathWeaponList));
+                    break;
+                case "6":
+                    break;
+                case "7":
+                    break;
+            }
+        }
+    }
+}
