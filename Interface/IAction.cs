65ceb1663da68bcc33bd3c5e1ae4c19faa62be78
Evgeny Kuznetsov <deadmaster.gm@yandex.ru>
2025-12-24 14:54:08 +0500

    Add interface IAction

---
+++ Interface/IAction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewGunShop.Interface
+{
+    internal interface IAction
+    {
+        void Buy();
+        void Sale();
+    }
+}
