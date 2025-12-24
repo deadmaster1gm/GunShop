34b091dcc069aee79bfeb2923049289493ba6a5f
Evgeny Kuznetsov <deadmaster.gm@yandex.ru>
2025-12-24 14:58:35 +0500

    Add class Program

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NewGunShop.Interface;
+using NewGunShop.List;
+using NewGunShop;
+
+IServiceCollection services = new ServiceCollection();
+services.AddTransient<GunShop>();
+services.AddTransient<IMainMenu, MainMenu>();
+services.AddTransient<ISwitchCasePointMenu, MainMenuSwitchCase>();
+services.AddTransient<IInputDataProcessor, ConsoleInputData>();
+services.AddTransient<IDataWeaponList, WeaponList>();
+services.AddTransient<IDataDollarBalance, DollarBalance>();
+
+var serviceProvider = services.BuildServiceProvider();
+
+var gunShopService = serviceProvider.GetRequiredService<GunShop>();
+while (true)
+{
+    gunShopService.GetMainMenu();
+}
+class GunShop
+{
+    private readonly IMainMenu _mainMenu;
+    private readonly ISwitchCasePointMenu _switchCasePointMenu;
+    private readonly IInputDataProcessor _inputDataProcessor;
+    private readonly IDataWeaponList _dataWeaponList;
+    private readonly IDataDollarBalance _dataDollarBalance;
+    public GunShop(IMainMenu mainMenu, ISwitchCasePointMenu switchCasePointMenu, IInputDataProcessor inputDataProcessor, IDataWeaponList dataWeaponList, IDataDollarBalance dataDollarBalance)
+    {
+        _mainMenu = mainMenu;
+        _switchCasePointMenu = switchCasePointMenu;
+        _inputDataProcessor = inputDataProcessor;
+        _dataWeaponList = dataWeaponList;
+        _dataDollarBalance = dataDollarBalance;
+    }
+    public void GetMainMenu()
+    {
+        _mainMenu.PointConsoleOutput(_dataDollarBalance.GetBalance());
+        _switchCasePointMenu.SwitchCase(_dataWeaponList, _inputDataProcessor);
+    }
+}
