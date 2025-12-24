f40ac9c1f93eab0a370f1a52526fb69b8389e1aa
Evgeny Kuznetsov <deadmaster.gm@yandex.ru>
2025-12-24 14:55:50 +0500

    Add class Weapon

---
+++ Weapon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NewGunShop.Interface;
+
+namespace NewGunShop
+{
+    public class Weapon : IWeapon
+    {
+        public string Model { get; set; }
+        public string Ammo { get; set; }
+        public string Price { get; set; }
+    }
+}
