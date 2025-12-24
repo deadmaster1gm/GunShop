97bb670a208054618bc373e13e11008f94aed314
Evgeny Kuznetsov <deadmaster.gm@yandex.ru>
2025-12-24 14:50:03 +0500

    Add interface IDataWeaponList

---
+++ Interface/IDataWeaponList.cs
@@ -0,0 +1,9 @@
+﻿namespace NewGunShop.Interface
+{
+    internal interface IDataWeaponList
+    {
+        void GetWeaponList(string path);
+        void AppendToWeaponList(string path, Weapon weapon);
+
+    }
+}
