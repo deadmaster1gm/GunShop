449ff8dd9b97827ade69d31eaedf0b64e9ef6539
Evgeny Kuznetsov <deadmaster.gm@yandex.ru>
2025-12-24 14:53:52 +0500

    Add interface IInputDataProcessor

---
+++ Interface/IInputDataProcessor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewGunShop.Interface
+{
+    internal interface IInputDataProcessor
+    {
+        Weapon SetData(string path);
+    }
+}
