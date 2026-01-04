724ec176bf01a20f4c04e6cfdf34e90ea3cac24d
Evgeny Kuznetsov <deadmaster.gm@yandex.ru>
2026-01-04 11:28:36 +0500

    Rename interface IAction to IItemAction

---
+++ Interface/IItemAction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewGunShop.Interface
+{
+    interface IItemAction
+    {
+        void Buy();
+        void Sale();
+    }
+}
