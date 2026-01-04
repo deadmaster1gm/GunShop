5dbab8e16d737cc7b8cc43e93a4bbbd9ac9668f9
Evgeny Kuznetsov <deadmaster.gm@yandex.ru>
2026-01-04 18:40:59 +0500

    Finalization of implementation method Buy (exception 0 count)

--- ItemAction.cs
+++ ItemAction.cs
@@ -10,47 +10,54 @@ namespace NewGunShop
 {
     class ItemAction : IItemAction
     {
-        public void Buy(List <Weapon> weaponList, string pathSeller, string pathBuyer, string pathDollarBalance)
+        public void Buy(List<Weapon> weaponList, string pathSeller, string pathBuyer, string pathDollarBalance)
         {
             while (true)
             {
-                int result;
-                string? input = Console.ReadLine();
-
-                if (int.TryParse(input, out result))
+                if (weaponList.Count == 0)
                 {
-                    Weapon weapon = weaponList[result - 1];
-                    List <Weapon> weaponListUser = JsonSerializer.Deserialize<List<Weapon>>(File.ReadAllText(pathBuyer));
-                    weaponListUser.Add(weapon);
-                    string jsonWeaponListUser = JsonSerializer.Serialize(weaponListUser);
-                    File.WriteAllText(pathBuyer, jsonWeaponListUser);
-
-                    weaponList.RemoveAt(result - 1);
-                    string jsonWeaponList = JsonSerializer.Serialize(weaponList);
-                    File.WriteAllText(pathSeller, jsonWeaponList);
-
-                    DollarBalance dollarBalance = JsonSerializer.Deserialize<DollarBalance>(File.ReadAllText(pathDollarBalance));
-                    int balanceSeller = dollarBalance._sellerDollarBalance;
-                    int balanceBuyer = dollarBalance._userDollarBalance;
-                    int priceWeapon = int.Parse(weapon.Price);
-                    int resultBalanceSeller = balanceSeller + priceWeapon;
-                    int resultBalanceBuyer = balanceBuyer - priceWeapon;
-                    dollarBalance._sellerDollarBalance = resultBalanceSeller;
-                    dollarBalance._userDollarBalance = resultBalanceBuyer;
-                    string jsonDollarBalance = JsonSerializer.Serialize(dollarBalance);
-                    File.WriteAllText(pathDollarBalance, jsonDollarBalance);
                     break;
                 }
                 else
                 {
-                    Console.Clear();
-                    Console.WriteLine("Некорректный ввод!");
+                    int result;
+                    string? input = Console.ReadLine();
+
+                    if (int.TryParse(input, out result))
+                    {
+                        Weapon weapon = weaponList[result - 1];
+                        List<Weapon> weaponListUser = JsonSerializer.Deserialize<List<Weapon>>(File.ReadAllText(pathBuyer));
+                        weaponListUser.Add(weapon);
+                        string jsonWeaponListUser = JsonSerializer.Serialize(weaponListUser);
+                        File.WriteAllText(pathBuyer, jsonWeaponListUser);
+
+                        weaponList.RemoveAt(result - 1);
+                        string jsonWeaponList = JsonSerializer.Serialize(weaponList);
+                        File.WriteAllText(pathSeller, jsonWeaponList);
+
+                        DollarBalance dollarBalance = JsonSerializer.Deserialize<DollarBalance>(File.ReadAllText(pathDollarBalance));
+                        int balanceSeller = dollarBalance._sellerDollarBalance;
+                        int balanceBuyer = dollarBalance._userDollarBalance;
+                        int priceWeapon = int.Parse(weapon.Price);
+                        int resultBalanceSeller = balanceSeller + priceWeapon;
+                        int resultBalanceBuyer = balanceBuyer - priceWeapon;
+                        dollarBalance._sellerDollarBalance = resultBalanceSeller;
+                        dollarBalance._userDollarBalance = resultBalanceBuyer;
+                        string jsonDollarBalance = JsonSerializer.Serialize(dollarBalance);
+                        File.WriteAllText(pathDollarBalance, jsonDollarBalance);
+                        break;
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Некорректный ввод!");
+                    }
                 }
+                Console.Clear();
+                Console.WriteLine("Успешно!");
+                Console.ReadLine();
+                Console.Clear();
             }
-            Console.Clear();
-            Console.WriteLine("Успешно!");
-            Console.ReadLine();
-            Console.Clear();
         }
 
         public void Sale()
--- List/WeaponList.cs
+++ List/WeaponList.cs
@@ -70,6 +70,8 @@ namespace NewGunShop.Interface
             {
                 Console.Clear();
                 Console.WriteLine("Пусто!");
+                Console.ReadLine();
+                Console.Clear();
             }
             else
             {
