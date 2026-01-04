5e9352b107273f9920da3ce9ccd954f4871dca8e
Evgeny Kuznetsov <deadmaster.gm@yandex.ru>
2026-01-04 12:46:09 +0500

    Add new class ItemAction

---
+++ ItemAction.cs
@@ -0,0 +1,36 @@
+﻿using NewGunShop.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace NewGunShop
+{
+    class ItemAction : IItemAction
+    {
+        public void Buy(List <Weapon> weaponList)
+        {
+            while (true)
+            {
+                int result;
+                string? input = Console.ReadLine();
+
+                if (int.TryParse(input, out result))
+                {
+                    Console.WriteLine("Успешно!");
+                    break;
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Некорректный ввод!");
+                }
+            }
+        }
+
+        public void Sale()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
